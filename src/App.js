f968cd4b367abbb34261e5f9a353c26cb0835409
thernthy <thernthy2003@gmail.com>
2024-03-21 17:38:10 +0700

    make new update on 21/03/2024

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './wigget/Navbar';
-
+import Navbar from './components/Navbar';
+import Bodycard from './components/Bodycard';
+import { useState } from 'react';
 function App() {
+  const [selectedLink, setSelectedLink] = useState('/home');
+  const [pageTitle, setPageTitle] = useState('모든 만화');
+  const handleMenuClick = (link, pageTitle) => {
+    setSelectedLink(link);
+    setPageTitle(pageTitle)
+  };
   return (
     <div className="App">
-        <header>
-          <Navbar />
+        <header className='fixed top-0 left-0 w-screen  z-50'>
+          <Navbar onMenuSwich={handleMenuClick} />
         </header>
+        <div className='body-wrapper pt-48 bg-slate-300'>
+          <Bodycard selectedLink={selectedLink} pageTile={pageTitle} onMenuSwich={handleMenuClick}/>
+        </div>
     </div>
   );
 }
