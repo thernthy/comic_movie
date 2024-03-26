ea8befc6185865553bd9418b2a2474ee2a039e94
thernthy <117651861+thernthy@users.noreply.github.com>
2024-03-26 20:03:09 +0700

    Update Oop.jsx
    Add oop component

---
+++ src/components/Oop.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+
+const Oop = () => {
+    return (
+       <>
+        <div className="flex flex-row 
+            justify-center
+            items-center 
+            w-screen h-screen">
+            <img src="#oop" alt="oop">
+        </div>
+       </>
+    )
+}
+
+export default Oop;
