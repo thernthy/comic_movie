a51aa26fb3f5d5b7c08b6e09ad1ec353f19f6a34
thernthy <thernthy2003@gmail.com>
2024-04-08 15:32:47 +0700

    add hoock file  for completed page

---
+++ src/hook/useHookCompleted.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { GetComicAxios } from "../Appcontrollers/comicXciosClient";
+
+export const useHookCompleted = () => {
+    const [page, setPage] = useState(1)
+    const [pageCount, setPageCount] = useState()
+    const fetchComic = async (page) => {
+        try {
+            const response = await GetComicAxios.get(`/comic/completed?page=${page}&pageRequest=24`, {
+                headers: {
+                    'X-API-Key' : process.env.REACT_APP_API_KEY
+                }
+            });
+      
+            setPageCount(response.data.last_page)
+            return response.data.data
+          } catch (error) {
+            // Handle errors here
+            console.error('Error:', error);
+            throw error;
+          }
+    }
+
+    const onSuccess = (data) => {
+        console.log("fetch succeeded!", data.data)
+    }
+
+    const onError = (error) => {
+        console.log("fetch failed!", error)
+    }
+    
+    const { data, isLoading, error, isError, refetch } = useQuery(
+        {
+            queryKey: ["completedComic", page],
+            queryFn: () => fetchComic(page),
+            staleTime: 1000,
+            onSuccess: onSuccess,
+            onError: onError,
+        }
+    )
+
+    return {data, isLoading, error, setPage, pageCount, refetch}
+}
